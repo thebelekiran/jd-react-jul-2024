6c413c7917c1b88700c5ca26f0b84ed7ebfccd4d
Prashanth Puranik <puranik@digdeeper.in>
2024-07-02 12:35:04 +0530

    child routing

---
+++ demos/day-02/react-router-v5-demo/src/components/workshops/WorkshopDetails/RequestSession/RequestSession.jsx
@@ -0,0 +1,10 @@
+const RequestSession = () => {
+    return (
+        <form>
+            <h1>Request for a new session</h1>
+            <div>Form comes in here</div>
+        </form>
+    );
+}
+
+export default RequestSession;
--- demos/day-02/react-router-v5-demo/src/components/workshops/WorkshopDetails/WorkshopDetails.jsx
+++ demos/day-02/react-router-v5-demo/src/components/workshops/WorkshopDetails/WorkshopDetails.jsx
@@ -1,6 +1,30 @@
+import { Link, Switch, Route } from 'react-router-dom';
+
+import SessionsList from './SessionsList/SessionsList';
+import RequestSession from './RequestSession/RequestSession';
+
 const WorkshopDetails = ({ id }) => {
     return (
-        <div>Show the details for workshop with id = {id}</div>
+        <>
+            <div>Show the details for workshop with id = {id}</div>
+            <h1>
+                Name of the workshop
+            </h1>
+            <div>Details....</div>
+
+            <div className="my-5">
+                <Link to={`/workshops/${id}`}>List of sessions</Link>
+                {" "}
+                <Link to={`/workshops/${id}/add`}>Request a new session</Link>
+            </div>
+
+            <div className="my-5">
+                <Switch>
+                    <Route path={`/workshops/:id/add`} component={RequestSession} />
+                    <Route path={`/workshops/:id`} component={SessionsList} />
+                </Switch>
+            </div>
+        </>
     );
 }
 
