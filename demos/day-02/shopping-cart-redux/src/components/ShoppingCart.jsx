6eac7b9b7a131fd7a048c903bfb484b9c6c14ea1
Prashanth Puranik <puranik@digdeeper.in>
2024-07-02 17:43:25 +0530

    redux completed

--- demos/day-02/shopping-cart-redux/src/components/ShoppingCart.jsx
+++ demos/day-02/shopping-cart-redux/src/components/ShoppingCart.jsx
@@ -1,7 +1,18 @@
+import { useSelector } from 'react-redux';
+
 const ShoppingCart = () => {
+    const items = useSelector((state) => state.cart.items);
+
     return (
         <>
             <div>Shopping Cart</div>
+            {
+                items.map(
+                    cartItem => (
+                        <div key={cartItem.id}>{cartItem.name}</div>
+                    )
+                )
+            }
         </>
     );
 }
