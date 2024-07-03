648aa100b6a589fda308fbb71192314613510feb
Prashanth Puranik <puranik@digdeeper.in>
2024-07-03 11:37:16 +0530

    so far

--- demos/day-03/workshops-app-next/src/pages/workshops/[id]/index.js
+++ demos/day-03/workshops-app-next/src/pages/workshops/[id]/index.js
@@ -1,13 +1,33 @@
-export function getServerSideProps(args) {
-    console.log(args);
+import axios from 'axios';
+
+export async function getServerSideProps(args) {
+    console.log(args); // args.params.id is the requested workshop's id
+
+    const id = args.params.id
+
+    // fetch data from `https://workshops-server.onrender.com/workshops/${id}/sessions`
+    const response = await axios.get(`https://workshops-server.onrender.com/workshops/${id}/sessions`);
+    const sessions = response.data;
 
     return {
         props: {
-
+            sessions: sessions
         }
     }
 }
 
-export default function WorkshopDetailsAndSessionsPage({ }) {
-    return <div>Workshop details</div>
+export default function WorkshopDetailsAndSessionsPage({ sessions }) {
+    return (
+        <div>
+            <h1>Sessions in this workshop</h1>
+            <hr />
+            {
+                sessions.map(
+                    s => (
+                        <div id={s.id}>{s.name}</div>
+                    )
+                )
+            }
+        </div>
+    );
 }
