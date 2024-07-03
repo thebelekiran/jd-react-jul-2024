841f2d02e7f97da550c2ca3156628f91d86ef8a8
Prashanth Puranik <puranik@digdeeper.in>
2024-07-03 14:04:23 +0530

    so far

--- demos/day-03/react-app-jwt/src/configureAxios.js
+++ demos/day-03/react-app-jwt/src/configureAxios.js
@@ -3,7 +3,8 @@ import axios from 'axios';
 function configureAxios() {
     // IN case you use fetch, you can create an interceptor-like feature following this - https://blog.logrocket.com/intercepting-javascript-fetch-api-requests-responses/
     axios.interceptors.request.use((config) => {
-        config.headers['Authorization'] = 'Bearer ' + localStorage('token');
+        config.headers['Authorization'] = 'Bearer ' + localStorage.getItem('token');
+        return config;
     });
 }
 
